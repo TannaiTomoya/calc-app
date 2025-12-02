29a1f84b9f60434940a013fa2328a98389102fcd
TannaiTomoya <tomobou5912@gmail.com>
2025-12-03 05:48:46 +0900

    計算アプリ課題完了

--- resources/views/livewire/calc.blade.php
+++ resources/views/livewire/calc.blade.php
@@ -17,14 +17,12 @@ $result = computed(function () {
     }
 
     $symbol = $operators[$this->operator];
-    
-    $value = match($this->operator) {
+
+    $value = match ($this->operator) {
         'addition' => $this->num1 + $this->num2,
         'subtraction' => $this->num1 - $this->num2,
         'multiplication' => $this->num1 * $this->num2,
-        'division' => $this->num2 != 0 
-            ? $this->num1 / $this->num2 
-            : 'エラー（0除算）',
+        'division' => $this->num2 != 0 ? $this->num1 / $this->num2 : 'エラー（0除算）',
         default => null,
     };
 
@@ -33,8 +31,7 @@ $result = computed(function () {
 
 ?>
 
-<div>
-    <h1>計算結果</h1>
-    <p>{{ $this->result }}</p>
+<div class="p-8">
+    <h1 class="text-5xl font-bold mb-6">計算結果</h1>
+    <p class="text-3xl">{{ $this->result }}</p>
 </div>
-
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// URLの num1/operator/num2 は calc コンポーネントの state（例: public int $num1;）へ自動的に注入される
 Volt::route('/calcs/{num1}/{operator}/{num2}', 'calc')
     ->name('calcs.show');
